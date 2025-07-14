b5903b8203160c6b5cfd383afb9ccac1004e1048
ogustavoress <ogustavoress@gmail.com>
2025-07-14 16:37:29 -0300

    Creating the model class 'Series'

---
+++ ogustavoress.com.github/model/Series.java
@@ -0,0 +1,17 @@
+public class Series extends Title {
+
+    public Series(String name, int releaseYear, String genre, int runningTime, String producedBy){
+        super(name, releaseYear, genre, runningTime, producedBy);
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Series: %s
+                Release Year: %d
+                Genre: %s
+                Running time: %d minutes
+                Produced by: %s
+                """.formatted(getName(), getReleaseYear(), getGenre(), getRunningTime(), getProducedBy());
+    }
+}
