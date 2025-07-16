1cface3c7f1a7cd94d57a82c4b38a796be0e7cba
ogustavoress <ogustavoress@gmail.com>
2025-07-16 04:34:50 -0300

    Creaitng the objects, creating & printing the list

--- ogustavoress.com.github/main/MainFavoritesList.java
+++ ogustavoress.com.github/main/MainFavoritesList.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+
 public class MainFavoritesList {
 
     public static void main(String[] args) {
         
+        Movie movie1 = new Movie("Ocean's 8", 2018);
+        Movie movie2 = new Movie("The House with a Clock in Its Walls", 2018);
+
+        Series series1 = new Series("The Big Bang Theory", 2007);
+        Series series2 = new Series("The Black List", 2013);
+        
+        Episode episode24S9 = new Episode("The Proposal Proposal", 24, 9, series1, 21);
+        Episode episode21S4 = new Episode("Mr. Kaplan", 21, 4, series2, 45);
+
+        ArrayList<Title> favoritesList = new ArrayList<>();
+        favoritesList.add(movie1);
+        favoritesList.add(movie2);
+        favoritesList.add(series1);
+        favoritesList.add(series2);
+        favoritesList.add(episode24S9);
+        favoritesList.add(episode21S4);
+
+
+        for (Title title : favoritesList) {
+            System.out.println(title);
+        }
+
     }
 
 }
